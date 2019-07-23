fbf4a603d514f9c7e7abc01b29c448a47174429e
lisheng <lisheng@coship.com>
2019-07-23 11:21:02 +0800

    preventDuplicateClicksWithDispose实现

--- base/src/main/java/com/enjoyandroid/base/ext/rx/RxView.kt
+++ base/src/main/java/com/enjoyandroid/base/ext/rx/RxView.kt
@@ -1,7 +1,12 @@
 package com.enjoyandroid.base.ext.rx
 
 import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import com.jakewharton.rxbinding3.view.clicks
+import com.uber.autodispose.AutoDispose.autoDisposable
+import com.uber.autodispose.ObservableSubscribeProxy
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
@@ -12,8 +17,7 @@ fun View.preventDuplicateClicks(): Observable<Unit> {
     return clicks().throttleFirst(500, TimeUnit.MILLISECONDS)
 }
 
-// todo 带自动取消绑定的防重复点击的扩展函数
-//fun View.preventDuplicateClicksWithDispose(owner: LifecycleOwner): AutoDisposeConverter<Unit>? {
-//    return preventDuplicateClicks()
-//        .`as`(AutoDispose.autoDisposable<Unit>(AndroidLifecycleScopeProvider.from(owner)).apply { Unit })
-//}
+ fun View.preventDuplicateClicksWithDispose(owner: LifecycleOwner): ObservableSubscribeProxy<Unit>? {
+     return preventDuplicateClicks()
+         .`as`<ObservableSubscribeProxy<Unit>?>(autoDisposable<Unit>((AndroidLifecycleScopeProvider.from(owner, Lifecycle.Event.ON_DESTROY))))
+}
