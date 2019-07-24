30c296002f389c9f1d806180ee81f2cc740dc184
lisheng <lisheng@coship.com>
2019-07-24 10:11:10 +0800

    去除rxlifecycle的依赖，采用object的方式写常量

--- base/src/main/java/com/enjoyandroid/base/common/BaseConstant.kt
+++ base/src/main/java/com/enjoyandroid/base/common/BaseConstant.kt
@@ -3,9 +3,6 @@ package com.kotlin.base.common
 /*
     基础常量
  */
-class BaseConstant{
-    companion object {
-        const val SERVER_ADDRESS = ""
-
-    }
+object BaseConstant {
+    const val SERVER_ADDRESS = ""
 }
